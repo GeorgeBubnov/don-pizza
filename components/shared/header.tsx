233b8aadf867643335fc8f299d9d51a4bcba812c
George <gopractic@gmail.com>
2025-05-02 17:23:56 +0300

    Create header

---
+++ components/shared/header.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import React from 'react';
+import { Container } from './container';
+import Image from 'next/image';
+import Link from 'next/link';
+import { SearchInput } from './search-input';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { ProfileButton } from './profile-button';
+
+interface Props {
+  hasSearch?: boolean;
+  hasCart?: boolean;
+  className?: string;
+}
+
+export const Header: React.FC<Props> = ({ hasSearch = true, hasCart = true, className }) => {
+  const router = useRouter();
+  const [openAuthModal, setOpenAuthModal] = React.useState(false);
+
+  const searchParams = useSearchParams();
+
+  React.useEffect(() => {
+    let toastMessage = '';
+
+    if (searchParams.has('paid')) {
+      toastMessage = 'Заказ успешно оплачен! Информация отправлена на почту.';
+    }
+
+    if (searchParams.has('verified')) {
+      toastMessage = 'Почта успешно подтверждена!';
+    }
+
+  }, []);
+
+  return (
+    <header className={cn('border-b', className)}>
+      <Container className="flex items-center justify-between py-8">
+        {/* Левая часть */}
+        <Link href="/">
+          <div className="flex items-center gap-4">
+            <Image src="/logo.png" alt="Logo" width={35} height={35} />
+            <div>
+              <h1 className="text-2xl uppercase font-black">Don Pizza</h1>
+              <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+            </div>
+          </div>
+        </Link>
+
+        {hasSearch && (
+          <div className="mx-10 flex-1">
+            <SearchInput />
+          </div>
+        )}
+
+        {/* Правая часть */}
+        <div className="flex items-center gap-3">
+
+          <ProfileButton onClickSignIn={() => setOpenAuthModal(true)} />
+
+        </div>
+      </Container>
+    </header>
+  );
+};

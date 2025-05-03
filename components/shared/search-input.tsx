d06c9dc273fd2f3352978666bb362cd7b32c2759
George <gopractic@gmail.com>
2025-05-03 22:02:56 +0300

    Add search popover

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -1,7 +1,10 @@
 'use client';
 
 import { cn } from '@/lib/utils';
+import { Api } from '@/services/api-client';
+import { Product } from '@prisma/client';
 import { Search } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react';
 import { useClickAway, useDebounce } from 'react-use';
 
@@ -12,15 +15,30 @@ interface Props {
 export const SearchInput: React.FC<Props> = ({ className }) => {
   const [searchQuery, setSearchQuery] = React.useState('');
   const [focused, setFocused] = React.useState(false);
+  const [products, setProducts] = React.useState<Product[]>([]);
   const ref = React.useRef(null);
 
   useClickAway(ref, () => {
     setFocused(false);
   });
 
+  useDebounce(
+    async () => {
+      try {
+        const response = await Api.products.search(searchQuery);
+        setProducts(response);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    250,
+    [searchQuery],
+  );
+
   const onClickItem = () => {
     setFocused(false);
     setSearchQuery('');
+    setProducts([]);
   };
 
   return (
@@ -40,6 +58,24 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
           onChange={(e) => setSearchQuery(e.target.value)}
         />
 
+        {products.length > 0 && (
+          <div
+            className={cn(
+              'absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30',
+              focused && 'visible opacity-100 top-12',
+            )}>
+            {products.map((product) => (
+              <Link
+                onClick={onClickItem}
+                key={product.id}
+                className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
+                href={`/product/${product.id}`}>
+                <img className="rounded-sm h-8 w-8" src={product.imageUrl} alt={product.name} />
+                <span>{product.name}</span>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );

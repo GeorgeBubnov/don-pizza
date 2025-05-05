21c8fcfb5ecf62edb16d369fb2a24a67792538d3
George <gopractic@gmail.com>
2025-05-06 00:03:03 +0300

    Update card item quantity in database

---
+++ store/cart.ts
@@ -0,0 +1,60 @@
+import { create } from "zustand";
+import { Api } from "../services/api-client";
+import { CartStateItem, getCartDetails } from "../lib/get-cart-details";
+import { CreateCartItemValues } from "../services/dto/cart.dto";
+
+export interface CartState {
+  loading: boolean;
+  error: boolean;
+  totalAmount: number;
+  items: CartStateItem[];
+
+  /* Получение товаров из корзины */
+  fetchCartItems: () => Promise<void>;
+
+  /* Запрос на обновление количества товара */
+  updateItemQuantity: (id: number, quantity: number) => Promise<void>;
+
+  /* Запрос на добавление товара в корзину */
+  addCartItem: (values: CreateCartItemValues) => Promise<void>;
+
+  /* Запрос на удаление товара из корзины */
+  removeCartItem: (id: number) => Promise<void>;
+}
+
+export const useCartStore = create<CartState>((set, get) => ({
+  items: [],
+  error: false,
+  loading: true,
+  totalAmount: 0,
+
+  fetchCartItems: async () => {
+    try {
+      set({ loading: true, error: false });
+      const data = await Api.cart.getCart();
+      set(getCartDetails(data));
+    } catch (error) {
+      console.error(error);
+      set({ error: true });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  updateItemQuantity: async (id: number, quantity: number) => {
+    try {
+      set({ loading: true, error: false });
+      const data = await Api.cart.updateItemQuantity(id, quantity);
+      set(getCartDetails(data));
+    } catch (error) {
+      console.error(error);
+      set({ error: true });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  removeCartItem: async (id: number) => {},
+
+  addCartItem: async (values: CreateCartItemValues) => {},
+}));

270db1a3c3107a4da81c8700030fe9805f63efff
George <gopractic@gmail.com>
2025-05-05 11:51:20 +0300

    Create route group

---
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -0,0 +1,20 @@
+import { prisma } from "@/prisma/prisma-client";
+import { notFound } from "next/navigation";
+
+export default async function ProductModalPage({ params: { id } }: { params: { id: string } }) {
+  const product = await prisma.product.findFirst({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      ingredients: true,
+      items: true,
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return <h1>{product.name}</h1>;
+}

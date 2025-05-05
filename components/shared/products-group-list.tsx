768c8e85499d292f618eeabd9701c8d4f8d99ec8
George <gopractic@gmail.com>
2025-05-05 10:04:16 +0300

    Fetch categories from database

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import { useIntersection } from "react-use";
 
 import { Title } from "./title";
-import { cn } from "@/lib/utils";
 import { ProductCard } from "./product-card";
 import { useCategoryStore } from "@/store";
 
@@ -26,7 +25,7 @@ export const ProductsGroupList: React.FC<Props> = ({
   const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
   const intersectionRef = React.useRef(null);
   const intersection = useIntersection(intersectionRef, {
-    threshold: 0.4,
+    threshold: 0.6,
   });
 
   React.useEffect(() => {

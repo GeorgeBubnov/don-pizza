28fc99d8d38490867675382f26dbb74de7edb55e
George <gopractic@gmail.com>
2025-05-06 14:18:45 +0300

    Display product description

--- components/shared/product-card.tsx
+++ components/shared/product-card.tsx
@@ -1,30 +1,38 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
-import { cn } from "@/lib/utils";
-import { Title } from "./title";
-import { CountButton } from "./count-button";
 import Link from "next/link";
+import React from "react";
+import { Title } from "./title";
+import { Button } from "../ui";
+import { Plus } from "lucide-react";
+import { Ingredient } from "@prisma/client";
 
 interface Props {
   id: number;
   name: string;
   price: number;
-  count?: number;
-  imageUrl?: string;
+  imageUrl: string;
+  ingredients: Ingredient[];
   className?: string;
 }
 
-export const ProductCard: React.FC<Props> = ({ id, name, price, count, imageUrl, className }) => {
+export const ProductCard: React.FC<Props> = ({
+  id,
+  name,
+  price,
+  imageUrl,
+  ingredients,
+  className,
+}) => {
   return (
-    <div className={cn(className)}>
+    <div className={className}>
       <Link href={`/product/${id}`}>
         <div className="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
-          <img className="w-[215px] h-[215px]" src={imageUrl} alt="Logo" />
+          <img className="w-[215px] h-[215px]" src={imageUrl} alt={name} />
         </div>
+
         <Title text={name} size="sm" className="mb-1 mt-3 font-bold" />
+
         <p className="text-sm text-gray-400">
-          Цыпленок, моцарелла, сыры чеддер и пармезан, сырный соус, томаты, соус альфредо, чеснок
+          {ingredients.map((ingredient) => ingredient.name).join(", ")}
         </p>
 
         <div className="flex justify-between items-center mt-4">
@@ -32,14 +40,10 @@ export const ProductCard: React.FC<Props> = ({ id, name, price, count, imageUrl,
             от <b>{price} ₽</b>
           </span>
 
-          {count ? (
-            <CountButton value={count} size="lg" />
-          ) : (
-            <Button variant="secondary">
-              <Plus className="w-4 h-4 mr-1" />
-              Добавить
-            </Button>
-          )}
+          <Button variant="secondary" className="text-base font-bold">
+            <Plus size={20} className="mr-1" />
+            Добавить
+          </Button>
         </div>
       </Link>
     </div>

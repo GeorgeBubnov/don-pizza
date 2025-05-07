7d9859bb52abb932d2225e5fb6adeeb404492c42
George <gopractic@gmail.com>
2025-05-07 10:46:57 +0300

    Create login modal

--- components/shared/profile-button.tsx
+++ components/shared/profile-button.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Button } from '../ui/button';
-import { CircleUser, User } from 'lucide-react';
-import Link from 'next/link';
+import { useSession } from "next-auth/react";
+import React from "react";
+import { Button } from "../ui/button";
+import { CircleUser, User } from "lucide-react";
+import Link from "next/link";
 
 interface Props {
   onClickSignIn?: () => void;
@@ -9,10 +10,16 @@ interface Props {
 }
 
 export const ProfileButton: React.FC<Props> = ({ className, onClickSignIn }) => {
+  const { data: session } = useSession();
 
   return (
     <div className={className}>
-      {(
+      {!session ? (
+        <Button onClick={onClickSignIn} variant="outline" className="flex items-center gap-1">
+          <User size={16} />
+          Войти
+        </Button>
+      ) : (
         <Link href="/profile">
           <Button variant="secondary" className="flex items-center gap-2">
             <CircleUser size={18} />
